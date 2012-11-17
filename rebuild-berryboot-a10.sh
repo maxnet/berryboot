8042eeb86ffe4fa8be720f602305d1fd9530af3c
Floris Bos <bos@je-eigen-domein.nl>
2012-11-17 03:01:29 +0100

    Build file for a10 devices

---
+++ rebuild-berryboot-a10.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# Rebuild Berryboot for Allwinner A10 devices
+#
+
+set -e
+
+# Normal rPi build first
+./rebuild-berryboot.sh
+
+# Make uInitrd
+mkimage -A arm -T ramdisk -C none -n "uInitrd" -d buildroot-2012.05/output/images/rootfs.cpio.gz output/uInitrd
+
+
+# Build kernel if it doesn't exist already
+if [ ! -e output/uImage ]; then
+	echo Building A10 kernel
+	git clone --depth=1 https://github.com/maxnet/linux-allwinner-aufs	
+	cd linux-allwinner-aufs
+	make sun4i_aufs_defconfig ARCH=arm
+	make -j4 uImage ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
+	make -j4 modules ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
+	make modules_install ARCH=arm INSTALL_MOD_PATH=output
+	cd output
+	# combine a10 modules with the ones in existing shared.tgz
+	tar xzf ../../output/shared.tgz
+	tar czf ../../output/shared.tgz *
+	cd ../..
+	cp linux-allwinner-aufs/arch/arm/boot/uImage output
+fi
+
+echo Build complete. Result is in \'output\' directory
+echo Note: you need to manually build u-boot (SPL) for your specific device, and copy script.bin to the output directory
+ 
