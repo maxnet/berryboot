d0ffd4a62be2a1b754c495b0459f2a4bef9614f9
Floris Bos <bos@je-eigen-domein.nl>
2012-11-28 21:14:26 +0100

    Added missing berrybootgui2 and rpi-userland buildroot packages

---
+++ buildroot-2012.05/package/berrybootgui2/berrybootgui2.mk
@@ -0,0 +1,31 @@
+#############################################################
+#
+# Berryboot GUI
+#
+#############################################################
+
+
+BERRYBOOTGUI2_VERSION=1.0
+BERRYBOOTGUI2_SITE=$(TOPDIR)/../BerrybootGUI2.0
+BERRYBOOTGUI2_SITE_METHOD=local
+BERRYBOOTGUI2_INSTALL_STAGING = NO
+BERRYBOOTGUI2_DEPENDENCIES=qt rpi-userland
+
+define BERRYBOOTGUI2_BUILD_CMDS
+	(cd $(@D) ; $(QT_QMAKE))
+	$(MAKE) -C $(@D) all
+	$(TARGET_STRIP) $(@D)/BerrybootInstaller
+endef
+
+define BERRYBOOTGUI2_INSTALL_TARGET_CMDS
+#	gzip -c $(@D)/BerrybootInstaller > $(TOPDIR)/output/images/BerrybootInstaller.gz
+        $(INSTALL) -m 0755 $(@D)/BerrybootInstaller $(TARGET_DIR)/usr/bin/BerrybootGUI
+        $(INSTALL) -D -m 0644 package/berrybootgui2/interfaces $(TARGET_DIR)/etc/network/interfaces
+        rm $(TARGET_DIR)/init || true
+        $(INSTALL) -m 0755 package/berrybootgui2/init $(TARGET_DIR)/init
+        $(INSTALL) -m 0755 $(STAGING_DIR)/opt/vc/lib/libvchiq_arm.so $(TARGET_DIR)/usr/lib
+        $(INSTALL) -m 0755 $(STAGING_DIR)/opt/vc/lib/libvcos.so $(TARGET_DIR)/usr/lib
+        $(INSTALL) -m 0755 $(STAGING_DIR)/opt/vc/lib/libbcm_host.so $(TARGET_DIR)/usr/lib
+endef
+
+$(eval $(call GENTARGETS))
---
+++ buildroot-2012.05/package/rpi-userland/rpi-userland.mk
@@ -0,0 +1,13 @@
+#############################################################
+#
+# Raspberry Pi userland libraries
+#
+#############################################################
+
+RPI_USERLAND_VERSION = HEAD
+RPI_USERLAND_SOURCE = rpi-userland-$(LIBFOO_VERSION).tar.gz
+RPI_USERLAND_SITE = git://github.com/raspberrypi/userland.git
+RPI_USERLAND_INSTALL_STAGING = YES
+RPI_USERLAND_INSTALL_TARGET = NO
+
+$(eval $(call CMAKETARGETS))
