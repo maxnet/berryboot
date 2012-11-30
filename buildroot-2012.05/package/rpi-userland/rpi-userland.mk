93132d1965b677e71de346d4baed7faee0b21c3a
Floris Bos <bos@je-eigen-domein.nl>
2012-11-30 03:50:55 +0100

    Preperation for Linux 3.6

--- buildroot-2012.05/package/rpi-userland/rpi-userland.mk
+++ buildroot-2012.05/package/rpi-userland/rpi-userland.mk
@@ -4,8 +4,8 @@
 #
 #############################################################
 
-RPI_USERLAND_VERSION = HEAD
-RPI_USERLAND_SOURCE = rpi-userland-$(LIBFOO_VERSION).tar.gz
+RPI_USERLAND_VERSION = next
+RPI_USERLAND_SOURCE = rpi-userland-$(RPI_USERLAND_VERSION).tar.gz
 RPI_USERLAND_SITE = git://github.com/raspberrypi/userland.git
 RPI_USERLAND_INSTALL_STAGING = YES
 RPI_USERLAND_INSTALL_TARGET = NO
