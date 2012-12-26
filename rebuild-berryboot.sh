4d96678660b47f12a850e456acfad842f21be84e
Floris Bos <bos@je-eigen-domein.nl>
2012-12-26 17:13:00 +0100

    Include /opt/vc/lib in shared.tgz

--- rebuild-berryboot.sh
+++ rebuild-berryboot.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+command -v mkimage >/dev/null || { echo "mkimage not found. Install u-boot-tools." ; exit 1; }
+
 if [ ! -e output/LICENSE.berryboot ]; then
 	cp -f LICENSE.berryboot output
 fi
@@ -22,5 +24,20 @@ cp output/images/shared.tgz ../output
 cp output/images/zImage ../output/kernel_rpi_aufs.img
 #cp output/images/rootfs.cpio.gz ../output/berryboot.img
 mkimage -A arm -T ramdisk -C none -n "uInitrd" -d output/images/rootfs.cpio.gz ../output/berryboot.img
+
+cd ..
+if [ ! -e output/start.elf ]; then
+	echo "Downloading Raspberry Pi firmware"
+	git clone --depth 1 git://github.com/raspberrypi/firmware.git 
+	cp firmware/boot/start*.elf output
+	cp firmware/boot/fixup*.dat output
+	cp firmware/boot/bootcode.bin output
+
+	# add /opt/vc binaries and libraries to shared.tgz. Leaving out examples
+	cd firmware/hardfp
+	rm -rf opt/vc/src
+	tar xzf ../../output/shared.tgz
+	tar czf ../../output/shared.tgz *
+fi
 
 echo Build complete. Result is in \'output\' directory 
