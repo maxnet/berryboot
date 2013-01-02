d25a6189caf0e6edd087090d5ab247beb5d1fff5
Floris Bos <bos@je-eigen-domein.nl>
2013-01-02 14:19:59 +0100

    Specify dependencies so that libraries are build before Berryboot

--- buildroot-2012.05/package/cryptsetup/cryptsetup.mk
+++ buildroot-2012.05/package/cryptsetup/cryptsetup.mk
@@ -9,7 +9,7 @@ CRYPTSETUP_SITE=https://cryptsetup.googlecode.com/files
 CRYPTSETUP_SOURCE=cryptsetup-$(CRYPTSETUP_VERSION).tar.bz2
 CRYPTSETUP_INSTALL_STAGING = YES
 CRYPTSETUP_INSTALL_TARGET = NO 
-CRYPTSETUP_DEPENDENCIES=lvm2 openssl popt
+CRYPTSETUP_DEPENDENCIES=lvm2 openssl popt util-linux
 CRYPTSETUP_CONF_OPT=--with-crypto_backend=kernel --enable-static-cryptsetup
 
 $(eval $(call AUTOTARGETS))
