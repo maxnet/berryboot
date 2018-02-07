7ab3dcdc1481c05bdef3f4ef0167bc0ecf089d2e
macmpi <spam@ipik.org>
2018-02-07 15:25:33 +0100

    New Pi foundation brcm firmware repo
    brcm firmware has just moved to https://github.com/RPi-Distro/firmware-nonfree/tree/master/brcm

--- buildroot-2015.02/package/rpi-wifi-firmware/rpi-wifi-firmware.mk
+++ buildroot-2015.02/package/rpi-wifi-firmware/rpi-wifi-firmware.mk
@@ -10,9 +10,9 @@ RPI_WIFI_FIRMWARE_LICENSE = Proprietary
 RPI_WIFI_FIRMWARE_LICENSE_FILES = brcm80211/LICENSE
 
 define RPI_WIFI_FIRMWARE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/brcm80211/brcm/brcmfmac43143.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43143.bin
-	$(INSTALL) -D -m 0644 $(@D)/brcm80211/brcm/brcmfmac43430-sdio.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43430-sdio.bin
-	$(INSTALL) -D -m 0644 $(@D)/brcm80211/brcm/brcmfmac43430-sdio.txt $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43430-sdio.txt
+	$(INSTALL) -D -m 0644 $(@D)/brcm/brcmfmac43143.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43143.bin
+	$(INSTALL) -D -m 0644 $(@D)/brcm/brcmfmac43430-sdio.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43430-sdio.bin
+	$(INSTALL) -D -m 0644 $(@D)/brcm/brcmfmac43430-sdio.txt $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43430-sdio.txt
 endef
 
 $(eval $(generic-package))
