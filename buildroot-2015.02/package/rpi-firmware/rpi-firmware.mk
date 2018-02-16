20ad16c8ae728f73a3aa025093d2e254193acaf3
Floris Bos <bos@je-eigen-domein.nl>
2018-02-16 23:33:03 +0100

    Switch rpi-firmware branch from stable to master

--- buildroot-2015.02/package/rpi-firmware/rpi-firmware.mk
+++ buildroot-2015.02/package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = stable
+RPI_FIRMWARE_VERSION = master
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
