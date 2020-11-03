e8e97973ea6dfecd2ce0c914f63575f5522079c0
Floris Bos <bos@je-eigen-domein.nl>
2020-11-03 16:17:03 +0100

    Add Pi 400 support
    - Update kernel to 5.4
    - Update wifi stuff

--- buildroot-2018.08/package/rpi-wifi-firmware/rpi-wifi-firmware.mk
+++ buildroot-2018.08/package/rpi-wifi-firmware/rpi-wifi-firmware.mk
@@ -4,14 +4,16 @@
 #
 ################################################################################
 
-RPI_WIFI_FIRMWARE_VERSION = 688531da4bcf802a814d9cb0c8b6d62e3b8a3327
-RPI_WIFI_FIRMWARE_SITE = $(call github,LibreELEC,brcmfmac_sdio-firmware-rpi,$(RPI_WIFI_FIRMWARE_VERSION))
+RPI_WIFI_FIRMWARE_VERSION = 5113681d6dcd46581a1882cbeb3d5cf1ddbf7676
+RPI_WIFI_FIRMWARE_SITE = $(call github,RPi-Distro,firmware-nonfree,$(RPI_WIFI_FIRMWARE_VERSION))
 RPI_WIFI_FIRMWARE_LICENSE = PROPRIETARY
 RPI_WIFI_FIRMWARE_LICENSE_FILES = LICENCE.broadcom_bcm43xx
 
 define RPI_WIFI_FIRMWARE_INSTALL_TARGET_CMDS
 	$(INSTALL) -d $(TARGET_DIR)/lib/firmware/brcm
-	$(INSTALL) -m 0644 $(@D)/firmware/brcm/brcmfmac* $(TARGET_DIR)/lib/firmware/brcm
+	$(INSTALL) -m 0644 $(@D)/brcm/brcmfmac43430* $(TARGET_DIR)/lib/firmware/brcm
+	$(INSTALL) -m 0644 $(@D)/brcm/brcmfmac43455* $(TARGET_DIR)/lib/firmware/brcm
+	$(INSTALL) -m 0644 $(@D)/brcm/brcmfmac43456* $(TARGET_DIR)/lib/firmware/brcm
 endef
 
 $(eval $(generic-package))
